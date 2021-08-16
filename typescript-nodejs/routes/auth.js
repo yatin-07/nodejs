e943295f1b82ef0a5d7c5001d59c4daffda6ee12
yatin-07 <nish07ravenclaw@gmail.com>
2021-08-16 21:31:46 +0530

    bcrypt added

--- typescript-nodejs/routes/auth.js
+++ typescript-nodejs/routes/auth.js
@@ -17,15 +17,24 @@ const authrouter = express_1.default.Router();
 const users_1 = __importDefault(require("../models/users"));
 const validate_1 = __importDefault(require("../models/validate"));
 require("joi");
-//const {registervalidation} = require('../models/validate')
+var bcrypt = require("bcryptjs");
 authrouter.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    //validating the data
     const { error } = validate_1.default.validate(req.body);
     if (error)
-        res.send(error.details[0].message);
+        return res.send(error.details[0].message);
+    //checking the user
+    const emailexist = yield users_1.default.findOne({ email: req.body.email });
+    if (emailexist)
+        return res.status(400).send("email alread exist");
+    //password
+    var salt = yield bcrypt.genSalt(10);
+    var hashPassword = yield bcrypt.hash(req.body.password, salt);
+    // creating a new user
     const user = new users_1.default({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try {
         const saveduser = yield user.save();
--- typescript-nodejs/routes/auth.ts
+++ typescript-nodejs/routes/auth.ts
@@ -3,17 +3,26 @@ const authrouter = express.Router();
 import Userschema from "../models/users";
 import schema from "../models/validate";
 import "joi";
+var bcrypt = require("bcryptjs");
 
-//const {registervalidation} = require('../models/validate')
 authrouter.post("/register", async (req: Request, res: Response) => {
-  const { error } =  schema.validate(req.body);
-  if (error)  res.send(error.details[0].message);
+  //validating the data
+  const { error } = schema.validate(req.body);
+  if (error) return res.send(error.details[0].message);
 
+  //checking the user
+  const emailexist = await Userschema.findOne({ email: req.body.email });
+  if (emailexist) return res.status(400).send("email alread exist");
+
+  //password
+  var salt = await bcrypt.genSalt(10);
+  var hashPassword = await bcrypt.hash(req.body.password, salt);
+
+  // creating a new user
   const user = new Userschema({
     name: req.body.name,
-    
     email: req.body.email,
-    password: req.body.password
+    password: hashPassword
   });
   try {
     const saveduser = await user.save();
